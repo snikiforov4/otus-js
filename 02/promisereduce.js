5974508ceff030fcdea2088e1b7fa69c2c2c2894
Serhii Nykyforov <04mailbox@gmail.com>
2018-03-03 18:03:01 +0200

    #hw-02: Add promiseReduce

---
+++ 02/promisereduce.js
@@ -0,0 +1,30 @@
+
+async function promiseReduce(promises, accumulator, initialValue) {
+    let result = initialValue;
+    for (let promise of promises) {
+        await promise.then(v => result = accumulator(result, v));
+    }
+    return result;
+}
+
+
+
+let promises = [
+    new Promise(resolve => setTimeout(() => resolve('Promises'), 500)),
+    Promise.resolve('are'),
+    new Promise(resolve => setTimeout(() => resolve('not'), 300)),
+    Promise.resolve('so easy'),
+];
+promiseReduce(promises, (p, c) => p ? `${p} ${c}` : c)
+    .then(v => console.log(v));
+// Promises are not so easy
+
+let numbers = [
+    new Promise(resolve => setTimeout(() => resolve(5), 500)),
+    Promise.resolve(2),
+    new Promise(resolve => setTimeout(() => resolve(3), 300)),
+    Promise.resolve(4),
+];
+promiseReduce(numbers, (p, c) => p ? p + c : c)
+    .then(v => console.log(v));
+// 14
