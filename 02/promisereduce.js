f1731569411c5eb3f3f14105effa06f7ff6b718d
Serhii Nykyforov <04mailbox@gmail.com>
2018-03-07 12:09:42 +0200

    #hw-02: Pass to promiseReduce functions that returns promises

--- 02/promisereduce.js
+++ 02/promisereduce.js
@@ -2,7 +2,7 @@
 async function promiseReduce(promises, accumulator, initialValue) {
     let result = initialValue;
     for (let promise of promises) {
-        await promise.then(v => result = accumulator(result, v));
+        await promise().then(v => result = accumulator(result, v));
     }
     return result;
 }
@@ -10,21 +10,21 @@ async function promiseReduce(promises, accumulator, initialValue) {
 
 
 let promises = [
-    new Promise(resolve => setTimeout(() => resolve('Promises'), 500)),
-    Promise.resolve('are'),
-    new Promise(resolve => setTimeout(() => resolve('not'), 300)),
-    Promise.resolve('so easy'),
+    () => new Promise(resolve => setTimeout(() => resolve('Promises'), 500)),
+    () => Promise.resolve('are'),
+    () => new Promise(resolve => setTimeout(() => resolve('not'), 300)),
+    () => Promise.resolve('so easy'),
 ];
 promiseReduce(promises, (p, c) => p ? `${p} ${c}` : c)
     .then(v => console.log(v));
 // Promises are not so easy
 
 let numbers = [
-    new Promise(resolve => setTimeout(() => resolve(5), 500)),
-    Promise.resolve(2),
-    new Promise(resolve => setTimeout(() => resolve(3), 300)),
-    Promise.resolve(4),
+    () => new Promise(resolve => setTimeout(() => resolve(5), 500)),
+    () => Promise.resolve(2),
+    () => new Promise(resolve => setTimeout(() => resolve(3), 300)),
+    () => Promise.resolve(4),
 ];
-promiseReduce(numbers, (p, c) => p ? p + c : c)
+promiseReduce(numbers, (p, c) => p ? p + c : c, 28)
     .then(v => console.log(v));
-// 14
+// 42
