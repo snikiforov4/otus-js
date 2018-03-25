92f7d8f50bd735fa81a266118da86779f50907a4
Serhii Nykyforov <04mailbox@gmail.com>
2018-03-25 23:29:16 +0300

    #hw-06: Save feed to mongodb

--- 06/server.js
+++ 06/server.js
@@ -2,22 +2,27 @@ const express = require('express');
 const utils = require('./utils');
 
 const Parser = require('rss-parser');
-// const MongoClient = require('mongodb').MongoClient;
-// const ObjectId = require('mongodb').ObjectID;
+const {MongoClient, ObjectID} = require('mongodb');
 
 const port = 8080;
 const rssURI = "https://www.eurekalert.org/rss/technology_engineering.xml";
-const rssItems = [];
-// const PERSONS = "persons";
+const dbUrl = 'mongodb://localhost:27017';
+const dbName = 'rss';
+
+const collectionName = "eureka";
 
 let app = express();
 app.use(utils.requestTimeLog);
+let db;
 
-// TODO code from lecture
-// MongoClient.connect("mongodb://localhost:27017", (err, db) => {
-//     if (err) return console.log(err);
-//     let dbo = db.db("mydb");
-//
+MongoClient.connect(dbUrl, (err, client) => {
+    if (err) {
+        console.log("Failed to establish connection to DB");
+        throw err;
+    }
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
+
 //     app.get('/person/:id', (req, res) => {
 //         dbo.collection(PERSONS).findOne({_id: new ObjectId(req.params.id)}, (err, doc) => {
 //             if (err) console.log(err);
@@ -32,20 +37,24 @@ app.use(utils.requestTimeLog);
 //             res.send(person);
 //         });
 //     });
-//
-//     app.listen(port, () => {
-//         console.log(`Server is running on http://localhost:${port}`);
-//     });
-// });
 
-const updateNews = function (uri) {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+    updateNews(db, rssURI);
+});
+
+const updateNews = function (db, uri) {
     let parser = new Parser();
     parser.parseURL(uri).then((feed) => {
-        feed.items.forEach(item => {
-            rssItems.push(item);
+        console.log("Updates has been received from RSS feed");
+        const feedItems = feed.items;
+        const collection = db.collection(collectionName);
+        collection.insertMany(feedItems, (err, result) => {
+            if (err) console.log("On saving received rss feed: %s", err);
+            console.log("Inserted %s documents into '%s'", result.ops.length, collectionName);
         });
-        console.log("Updates has been received from RSS feed")
-    }).catch(() => console.log("On rss feed update: %s", e));
+    }).catch((err) => console.log("On rss feed update: %s", err));
 };
 
 app.get('/news/:id', (req, res) => {
@@ -60,9 +69,4 @@ app.get('/news/:id', (req, res) => {
 
 app.get('/news', (req, res) => {
     res.send(rssItems);
-});
-
-updateNews(rssURI);
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
 });
