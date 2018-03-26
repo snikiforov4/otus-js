7e13c357e299fd7f2e9714f417f4f65ca8c2301a
Serhii Nykyforov <04mailbox@gmail.com>
2018-03-26 22:44:19 +0300

    #hw-07: Add test checking passing any parameter to next func

--- 07/__tests__/series.test.js
+++ 07/__tests__/series.test.js
@@ -6,6 +6,20 @@ test('call each function passed as argument', () => {
     const fn2 = jest.fn().mockImplementation(next => next()).mockName('fn2');
     return series(fn1, fn2).then(() => {
         expect(fn1).toHaveBeenCalled();
-        expect(fn2).toHaveBeenCalled()
+        expect(fn2).toHaveBeenCalled();
+    });
+});
+
+test('directly call last function on passing any parameter to next()', () => {
+    expect.assertions(4);
+    const passed = jest.fn().mockImplementation(next => next()).mockName('passed');
+    const skipped = jest.fn().mockImplementation(next => next()).mockName('skipped');
+    const failed = jest.fn().mockImplementation(next => next('fail')).mockName('failed');
+    const last = jest.fn().mockImplementation(next => next()).mockName('last');
+    return series(passed, passed, failed, skipped, skipped, last).then(() => {
+        expect(passed).toHaveBeenCalledTimes(2);
+        expect(skipped).toHaveBeenCalledTimes(0);
+        expect(failed).toHaveBeenCalledTimes(1);
+        expect(last).toHaveBeenCalledTimes(1);
     });
 });
--- 07/series.js
+++ 07/series.js
@@ -8,18 +8,15 @@ const series = async function (...functions) {
 };
 
 const innerFunctionCall = function (...restFunctions) {
-    console.log("inner");
-    return restFunctions.shift()(next(restFunctions));
+    const nextFunction = restFunctions.shift();
+    return nextFunction(next(...restFunctions));
 };
 
 const next = function (...restFunctions) {
     return function () {
         if (arguments.length > 0) {
             if (restFunctions.length > 0) {
-                return restFunctions[restFunctions.length - 1]([]);
-            } else {
-                let err = arguments[0];
-                return Promise.reject(err);
+                return innerFunctionCall(restFunctions[restFunctions.length - 1]);
             }
         } else {
             if (restFunctions.length > 0) {
