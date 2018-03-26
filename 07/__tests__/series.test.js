2703f29dd7ac5cd17f090130620f703e676ad2f9
Serhii Nykyforov <04mailbox@gmail.com>
2018-03-26 19:40:35 +0300

    #hw-07: Make refactoring

--- 07/__tests__/series.test.js
+++ 07/__tests__/series.test.js
@@ -2,8 +2,8 @@ const series = require('../series');
 
 test('call each function passed as argument', () => {
     expect.assertions(2);
-    const fn1 = jest.fn();
-    const fn2 = jest.fn();
+    const fn1 = jest.fn().mockImplementation(next => next()).mockName('fn1');
+    const fn2 = jest.fn().mockImplementation(next => next()).mockName('fn2');
     return series(fn1, fn2).then(() => {
         expect(fn1).toHaveBeenCalled();
         expect(fn2).toHaveBeenCalled()
--- 07/series.js
+++ 07/series.js
@@ -1,6 +1,31 @@
 const series = async function (...functions) {
-    for (let nextFunction of functions) {
-        nextFunction();
+    if (functions.length > 0) {
+        return new Promise(resolve => {
+            innerFunctionCall(...functions);
+            resolve();
+        });
+    }
+};
+
+const innerFunctionCall = function (...restFunctions) {
+    console.log("inner");
+    return restFunctions.shift()(next(restFunctions));
+};
+
+const next = function (...restFunctions) {
+    return function () {
+        if (arguments.length > 0) {
+            if (restFunctions.length > 0) {
+                return restFunctions[restFunctions.length - 1]([]);
+            } else {
+                let err = arguments[0];
+                return Promise.reject(err);
+            }
+        } else {
+            if (restFunctions.length > 0) {
+                return innerFunctionCall(...restFunctions);
+            }
+        }
     }
 };
 
